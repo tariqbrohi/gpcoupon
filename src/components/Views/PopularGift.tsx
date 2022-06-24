d8eb237e9866861454fa7a1117e2c97f65c15b67
steven pinker <steven@gpointwallet.com>
2022-06-24 16:50:57 -0700

    ok

--- src/components/Views/PopularGift.tsx
+++ src/components/Views/PopularGift.tsx
@@ -40,49 +40,57 @@ const PopularGift = (props: any) => {
       setData(data);
     })();
   }, [country]);
-
   return (
     <div className={classes.carousaldiv}>
       <h2>🎁 Most Popular Gifts 🎁</h2>
-      {/* {Data?.length !== 0 && (
-        <Carousel
-          swipeable={true}
-          draggable={true}
-          autoPlay={false}
-          shouldResetAutoplay={false}
-          showDots={false}
-          responsive={responsive}
-          infinite={true}
-          removeArrowOnDeviceType={[`mobile`]}
-          deviceType={props.deviceType}
-          className={classes.carousal}
-        >
-          {Data?.map((item: any, index: number) => (
-            <div
-              key={index}
-              className={classes.imageDiv}
-              onClick={() => {
-                setSingleVoucher(item);
-                Router.push({
-                  pathname: `/item/${item.name}`,
-                });
-              }}
-            >
-              <div className={classes.card}>
-                <div style={{ borderRadius: `16px` }} className={classes.image}>
-                  <img
-                    alt={`image`}
-                    src={item.imageUrl}
-                    width={`100%`}
-                    height="150px"
-                  />
-                </div>
-                <p className={classes.title}>{item.name}</p>
+      <Carousel
+        swipeable={false}
+        draggable={false}
+        autoPlay={false}
+        showDots={true}
+        responsive={responsive}
+        infinite={true}
+        removeArrowOnDeviceType={[`mobile`]}
+        deviceType={props.deviceType}
+        className={classes.carousal}
+        // swipeable={true}
+        // draggable={true}
+        // autoPlay={false}
+        // shouldResetAutoplay={false}
+        // showDots={false}
+        // responsive={responsive}
+        // infinite={true}
+        // removeArrowOnDeviceType={[`mobile`]}
+        // deviceType={props.deviceType}
+        // className={classes.carousal}
+      >
+        {Data?.map((item: any, index: number) => (
+          <div
+            key={index}
+            className={classes.imageDiv}
+            onClick={() => {
+              setSingleVoucher(item);
+              Router.push({
+                pathname: `/item/${item?.name}`,
+              });
+            }}
+          >
+            <div className={classes.card}>
+              <div style={{ borderRadius: `16px` }} className={classes.image}>
+                <img
+                  alt={`image`}
+                  src={item?.imageUrl}
+                  width={`100%`}
+                  height="150px"
+                />
               </div>
+              <p className={classes.title}>{item?.name}</p>
             </div>
-            // <Item key={index} item={data} />
-          ))}
-        </Carousel>
+          </div>
+        ))}
+      </Carousel>
+      {/* {Data?.length !== 0 && (
+        
       )} */}
     </div>
   );
