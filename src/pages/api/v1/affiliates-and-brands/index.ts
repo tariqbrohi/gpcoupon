d55522102dae109d35a59b411fa409608efaf1dd
Seungjoo Kim <sjskybone@gmail.com>
2022-10-11 14:55:47 -0700

    GPN-135 Created affs-and-brands folder in the v1.

---
+++ src/pages/api/v1/affiliates-and-brands/index.ts
@@ -0,0 +1,38 @@
+import errorHandler from '@/pages/api/_middlewares/error-handler';
+import prisma from '@/prisma';
+import { NotFoundError } from '@/lib/errors';
+
+export default errorHandler(async function handler(req, res) {
+  const method = req.method;
+
+  if (method !== 'get') {
+    throw new NotFoundError();
+  }
+
+  const {
+    // affiliate = 'false',
+    country,
+    status = 'AVAILABLE',
+  } = req.query as any;
+
+  let where: Record<string, any> = {
+    status,
+    // ...(affiliate === 'true' ? { affiliate: true } : {}),
+  };
+
+  if (status === 'ALL') {
+    delete where.status;
+  }
+
+  if (country) {
+    where.countries = {
+      has: country,
+    };
+  }
+
+  const brands = await prisma.brand.findMany({
+    where,
+  });
+
+  res.send(brands);
+});
