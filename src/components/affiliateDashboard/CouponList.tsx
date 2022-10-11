5ceadd3a6b4a85a57297df2413ffdff18c845661
Seongoh <seongohryoo@gmail.com>
2022-10-11 14:23:32 -0700

    UI design

--- src/components/affiliateDashboard/CouponList.tsx
+++ src/components/affiliateDashboard/CouponList.tsx
@@ -6,14 +6,14 @@ import { useGetAffiliatesQuery } from "@/services";
 
 const TAKE = 20;
 
-export default function CouponList() {
+export default function CouponList(props: any) {
+  const {tmpData} = props;
   const { country } = useContext(AppContext);
   const { data, loading } = useGetAffiliatesQuery({
     data: {
       country,
     },
   });
-  
 
   return (
     <>
@@ -21,18 +21,45 @@ export default function CouponList() {
         <Table.Head>
           <Table.Row>
             <Table.HeadCell>Name</Table.HeadCell>
-            <Table.HeadCell>Price</Table.HeadCell>
-            <Table.HeadCell>Discount</Table.HeadCell>
-            <Table.HeadCell>Status</Table.HeadCell>
-            <Table.HeadCell>Coupon Image</Table.HeadCell>
-            <Table.HeadCell>Created Date</Table.HeadCell>
+            <Table.HeadCell>Description</Table.HeadCell>
+            <Table.HeadCell>Creation Date</Table.HeadCell>
             <Table.HeadCell>Expire Date</Table.HeadCell>
+            <Table.HeadCell>Qty</Table.HeadCell>
+            <Table.HeadCell>Original Price</Table.HeadCell>
+            <Table.HeadCell>Discounted Price</Table.HeadCell>
+            <Table.HeadCell>My Profit</Table.HeadCell>
           </Table.Row>
         </Table.Head>
         <Table.Body>
-          {console.log(data)}     
+          <Table.Row>
+            <Table.Cell positive>Total</Table.Cell>
+            <Table.Cell positive>-</Table.Cell>
+            <Table.Cell positive>-</Table.Cell>
+            <Table.Cell positive>-</Table.Cell>
+            <Table.Cell positive>{tmpData.total.count}</Table.Cell>
+            <Table.Cell positive>-</Table.Cell>
+            <Table.Cell positive>-</Table.Cell>
+            <Table.Cell positive>${tmpData.total.discount_sum * 0.8}</Table.Cell>
+          </Table.Row>
+          {
+            tmpData.data.map((d: any, idx: number) =>{
+              return (
+                <Table.Row
+                  key={idx}
+                >
+                  <Table.Cell>{d.name}</Table.Cell>
+                  <Table.Cell>{d.Description}</Table.Cell>
+                  <Table.Cell>{d.creationDate}</Table.Cell>
+                  <Table.Cell>{d.ExpireDate}</Table.Cell>
+                  <Table.Cell>1</Table.Cell>
+                  <Table.Cell>${d.originalPrice}</Table.Cell>
+                  <Table.Cell>${d.discountedPrice}</Table.Cell>
+                  <Table.Cell>${d.discountedPrice * 0.8}</Table.Cell>
+                </Table.Row>
+              )
+            })
+          }
         </Table.Body>
-
       </Table>
     </>
   )
--- src/pages/affiliateDashboard/index.tsx
+++ src/pages/affiliateDashboard/index.tsx
@@ -1,3 +1,5 @@
+import AppContext from '@/modules/components/AppContext';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import AppContainer from '@/layouts/AppContainer';
 import AppHeader from '@/layouts/AppHeader';
 import AppFooter from '@/layouts/AppFooter';
@@ -5,26 +7,64 @@ import AppMain from '@/layouts/AppMain';
 import AppNav from '@/layouts/AppNav';
 import CategoryList from '@/components/categories/CategoryList';
 import Head from '@/modules/components/Head';
-import React from 'react';
 import Search from '@/modules/components/Search';
 import { Paragraph, Spacer } from '@growth-ui/react';
 // import BrandList from '@/components/brands/BrandList';
 // import DashboardList from '@/components/affiliateDashboard/DashboardList';
 import CouponList from '@/components/affiliateDashboard/CouponList';
+// import { useGetAffiliateItemsForDashboardQuery, useGetAffiliateItemsForDashboardLazyQuery} from '@/services';
+
+const TAKE = 10;
 
 export default function AffiliateDashboard() {
+  const { user, country } = useContext(AppContext);
+  const [ sortBy, setSortBy ] = useState('createdAt,desc');
+  // const [ query, { data, loading }] = useGetAffiliateItemsForDashboardLazyQuery({});
+
+  // useEffect(() => {
+  //   if (user !== null) {
+  //     query({
+  //       data: {
+  //         country, 
+  //         sub: user!.id,
+  //         sortBy,
+  //       }
+  //     });
+  //   }
+  // }, [sortBy]);
+
+
   return (
     <>
       <Head title="GCoupon | AffiliateDashboard" />
       <AppHeader bgTransition={false} />
+      {/* { data &&
+        console.log('api call', data)} */}
       <AppMain>
         <AppContainer>
           <Paragraph fontWeight={700} fontSize={26}>
-            My Dashboard
+            My Coupon
           </Paragraph>
-          <CouponList />
+          <CouponList 
+            tmpData={tmpData}
+          />
         </AppContainer>
       </AppMain>
     </>
   );
 }
+
+const tmpData = {
+  total: {
+    count: 1,
+    discount_sum: 80
+  },
+  data: [
+  {
+    name: "gyrodrop",
+    Description: "tmp",
+    creationDate: "07/08/2013",
+    ExpireDate: "07/08/2023",
+    originalPrice: 1000,
+    discountedPrice: 80,
+  }]};
