94a0fb41dfbb4e0b6a39ae6cb0691f69d559965e
Seongoh <seongohryoo@gmail.com>
2022-10-27 11:41:21 -0700

    remove unnecessary code and comment

--- src/pages/api/v1/items/searchbar.ts
+++ src/pages/api/v1/items/searchbar.ts
@@ -2,7 +2,6 @@ import errorHandler from "../../_middlewares/error-handler";
 import prisma from "@/prisma";
 import xoxoday from "../../_lib/xoxoday";
 import { NotFoundError, BadRequestError } from "@/lib/errors";
-import items from "../../admin/items";
 
 export default errorHandler(async function handler(req, res) {
     const method = req.method;
@@ -45,18 +44,3 @@ export default errorHandler(async function handler(req, res) {
         res.send(items);
     }
 })
-
-
-    
-
-
-
-
-
-
-// https://www.prisma.io/docs/concepts/components/prisma-client/filtering-and-sorting#filtering
-    // Search by item name.
-    // contains key field is way to go.
-    // set mode to insensitive (https://www.prisma.io/docs/concepts/components/prisma-client/filtering-and-sorting#case-insensitive-filtering)
-    // return resulst
-    // e.g) res.send({ items, total });
