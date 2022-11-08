71401c0390d129e2fb8b694d1d6c61c0e477ab49
kyuhakyuk <kyuhak.yuk@icloud.com>
2022-11-08 15:04:12 -0800

    ok

--- src/pages/api/v1/redeem.ts
+++ src/pages/api/v1/redeem.ts
@@ -1,5 +1,4 @@
 import errorHandler from '@/pages/api/_middlewares/error-handler';
-import grapherjs from '../_lib/grapherjs';
 import moment from 'moment';
 import prisma from '@/prisma';
 import { isNil } from 'lodash';
@@ -11,101 +10,99 @@ import {
 } from '@/lib/errors';
 import { Item } from '@prisma/client';
 
-export default errorHandler(
-  grapherjs.withTracingHandler(async function handler(req, res) {
-    const method = req.method;
-    prisma.item.aggregateRaw({
-      pipeline: [{ $match: { brandId: 'ckqj2q2xw0000jx9x9x9x9x9x' } }],
-    });
-    if (method !== 'post') {
-      throw new NotFoundError();
-    }
+export default errorHandler(async function handler(req, res) {
+  const method = req.method;
+  prisma.item.aggregateRaw({
+    pipeline: [{ $match: { brandId: 'ckqj2q2xw0000jx9x9x9x9x9x' } }],
+  });
+  if (method !== 'post') {
+    throw new NotFoundError();
+  }
 
-    const { amount, code, pin, sub, orderId, t } = req.body;
+  const { amount, code, pin, sub, orderId, t } = req.body;
 
-    if (t !== 'thisisfortemporaryshit') {
-      throw new ForbiddenError();
-    }
+  if (t !== 'thisisfortemporaryshit') {
+    throw new ForbiddenError();
+  }
 
-    const gift = await prisma.gift.findUnique({
-      where: {
-        code_pin: {
-          code,
-          pin,
-        },
+  const gift = await prisma.gift.findUnique({
+    where: {
+      code_pin: {
+        code,
+        pin,
       },
-      include: {
-        order: true,
-      },
-    });
+    },
+    include: {
+      order: true,
+    },
+  });
 
-    if (!gift || gift.order.id !== orderId) throw new NotFoundError('');
+  if (!gift || gift.order.id !== orderId) throw new NotFoundError('');
 
-    const { item } = gift.order;
+  const { item } = gift.order;
 
-    // if gift is expired
-    if (
-      gift.status === 'expired' ||
-      moment().unix() >=
-        moment(gift.order.createdAt * 1000)
-          .add((item as Item).expiresIn, 'days')
-          .unix()
-    ) {
-      throw new BadRequestError(
-        `Gift expired at ${moment(gift.order.createdAt * 1000)
-          .add((item as Item).expiresIn, 'days')
-          .utc()
-          .format('MM-DD-YYYY hh:mm:ss (UTC)')}`,
-      );
-    }
+  // if gift is expired
+  if (
+    gift.status === 'expired' ||
+    moment().unix() >=
+      moment(gift.order.createdAt * 1000)
+        .add((item as Item).expiresIn, 'days')
+        .unix()
+  ) {
+    throw new BadRequestError(
+      `Gift expired at ${moment(gift.order.createdAt * 1000)
+        .add((item as Item).expiresIn, 'days')
+        .utc()
+        .format('MM-DD-YYYY hh:mm:ss (UTC)')}`,
+    );
+  }
 
-    // if gift is already used.
-    if (gift.status === 'used') throw new BadRequestError('Gift already used');
+  // if gift is already used.
+  if (gift.status === 'used') throw new BadRequestError('Gift already used');
 
-    // If requested gift detail is different thant what is in db.
-    if ((item as any).brand?.sub !== sub) {
-      throw new BadRequestError('');
-    }
+  // If requested gift detail is different thant what is in db.
+  if ((item as any).brand?.sub !== sub) {
+    throw new BadRequestError('');
+  }
 
-    if ((item as Item).type === 'GIFT_ICON') {
-      await prisma.gift.update({
-        where: {
-          id: gift.id,
-        },
-        data: {
-          status: 'used',
-          updatedAt: moment().unix(),
-        },
-      });
-
-      return res.send(item);
-    }
-
-    // For GIFT_CARD
-    if (isNil(gift.amount)) {
-      // todo
-      // slack
-
-      throw new InternalServerError();
-    }
-
-    const diff = +amount - gift.amount;
-
+  if ((item as Item).type === 'GIFT_ICON') {
     await prisma.gift.update({
       where: {
         id: gift.id,
       },
       data: {
-        amount: diff >= 0 ? 0 : diff * -1,
+        status: 'used',
         updatedAt: moment().unix(),
-        status: diff >= 0 ? 'used' : 'available',
       },
     });
 
-    res.send({
-      remainingAmountToPay: diff < 0 ? 0 : diff,
-      remainingBalance: diff >= 0 ? 0 : diff * -1,
-    });
-    // res.send(true);
-  }),
-);
+    return res.send(item);
+  }
+
+  // For GIFT_CARD
+  if (isNil(gift.amount)) {
+    // todo
+    // slack
+
+    throw new InternalServerError();
+  }
+
+  const diff = +amount - gift.amount;
+
+  await prisma.gift.update({
+    where: {
+      id: gift.id,
+    },
+    data: {
+      amount: diff >= 0 ? 0 : diff * -1,
+      updatedAt: moment().unix(),
+      status: diff >= 0 ? 'used' : 'available',
+    },
+  });
+
+  res.send({
+    remainingAmountToPay: diff < 0 ? 0 : diff,
+    remainingBalance: diff >= 0 ? 0 : diff * -1,
+  });
+  // res.send(true);
+});
