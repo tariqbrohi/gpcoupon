b660ccb5cec2cb8da8ebd18b49696b21b3042060
Seungjoo Kim <sjskybone@gmail.com>
2022-11-08 16:37:34 -0800

    GPN-255 Added Icon on the Admin Sidebar.

--- src/components/sidebar/AdminSidebar.tsx
+++ src/components/sidebar/AdminSidebar.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { ROUTES } from '@/ROUTES';
 import { useRouter } from 'next/router';
-import { Collapse, Image, List, Sidebar, SidebarProps, Spacer } from '@growth-ui/react';
+import { Collapse, Icon, List, Sidebar, SidebarProps, Spacer } from '@growth-ui/react';
+import SvgDashboard from '@/icons/dashboard';
+import SvgCoupon from '@/icons/coupon';
+import SvgBrand from '@/icons/brand';
 
 export default function AdminSidebar(props: SidebarProps) {
   const { pathname, push } = useRouter();
@@ -15,90 +18,23 @@ export default function AdminSidebar(props: SidebarProps) {
   return (
     <Sidebar visible animation="push" {...props}>
       <List padded verticalAlign="middle" style={{ margin: '0 15px' }}>
-        {/* Coupon Items */}
-        {/* <List.Item active={activeItem === 'items'} style={styles.listItem}>
-          <List.Content>Coupon</List.Content>
-        </List.Item>
-        <Collapse expanded>
-          <List padded selection verticalAlign="middle">
-            <List.Item
-              active={pathname === ROUTES.admin.createItem}
-              onClick={handleClickItem(ROUTES.admin.createItem)}
-              style={styles.collapseItem}
-            >
-              <List.Content>Request Coupon</List.Content>
-            </List.Item>
-            <List.Item
-              active={pathname === ROUTES.admin.items}
-              onClick={handleClickItem(ROUTES.admin.items)}
-              style={styles.collapseItem}
-            >
-              <List.Content>List Coupon</List.Content>
-            </List.Item>
-          </List>
-        </Collapse> */}
-
-        {/* Brands */}
-        {/* <List.Item active={activeItem === 'brands'} style={styles.listItem}>
-          <List.Content>Brand</List.Content>
-        </List.Item>
-        <Collapse expanded>
-          <List padded selection verticalAlign="middle">
-            <List.Item
-              active={pathname === ROUTES.admin.createBrand}
-              onClick={handleClickItem(ROUTES.admin.createBrand)}
-              style={styles.collapseItem}
-            >
-              <List.Content>Request Brand</List.Content>
-            </List.Item>
-            <List.Item
-              active={pathname === ROUTES.admin.listBrands}
-              onClick={handleClickItem(ROUTES.admin.listBrands)}
-              style={styles.collapseItem}
-            >
-              <List.Content>List Brand</List.Content>
-            </List.Item>
-          </List>
-        </Collapse> */}
-
-        {/* GPOINTS */}
-        {/* <List.Item active={activeItem === 'gpoints'} style={styles.listItem}>
-          <List.Content>GPoints</List.Content>
-        </List.Item>
-        <Collapse expanded>
-          <List padded selection verticalAlign="middle">
-            <List.Item
-              active={pathname === ROUTES.admin.createGpoint}
-              onClick={handleClickItem(ROUTES.admin.createGpoint)}
-              style={styles.collapseItem}
-            >
-              <List.Content>Request GPoint</List.Content>
-            </List.Item>
-            <List.Item
-              active={pathname === ROUTES.admin.listGpoints}
-              onClick={handleClickItem(ROUTES.admin.listGpoints)}
-              style={styles.collapseItem}
-            >
-              <List.Content>List GPoints</List.Content>
-            </List.Item>
-
-            <List.Item
-              active={pathname === ROUTES.admin.orders}
-              onClick={handleClickItem(ROUTES.admin.orders)}
-              style={styles.collapseItem}
-            >
-              <List.Content>Orders</List.Content>
-            </List.Item>
-          </List>
-        </Collapse> */}
-        
-        
-
         {/* Admin Dashboard */}
-        <List.Item active={activeItem === 'admin'} style={styles.listItem}>
-          <List.Content>Admin</List.Content>
+        <List.Item 
+          active={pathname === ROUTES.admin.adminDashboard}
+          onClick={handleClickItem(ROUTES.admin.adminDashboard)}
+          // active={activeItem === 'admin'} 
+          style={styles.listItem}
+        >
+          <div style={{display: "flex", alignItems: "center", cursor: "pointer"}}>
+            {/* <SvgDashboard /> */}
+            <Icon name='newspaper' color='black' />
+            <Spacer size={10} />
+            <List.Content>Admin Dashboard</List.Content>
+          </div>
         </List.Item>
-        <Collapse expanded>
+        <Spacer size={30} />
+
+        {/* <Collapse expanded>
           <List padded selection verticalAlign="middle">
             <List.Item
               active={pathname === ROUTES.admin.adminDashboard}
@@ -108,11 +44,13 @@ export default function AdminSidebar(props: SidebarProps) {
               <List.Content>Admin Dashboard</List.Content>
             </List.Item>
           </List>
-        </Collapse>
+        </Collapse> */}
 
+        {/* Coupon */}
         <List.Item active={activeItem === 'items'} style={styles.listItem}>
           <div style={{display: "flex", alignItems: "center"}}>
-            <Image src='/images/admin_list_icons/coupon.png' alt='coupon' style={{width: "30px"}} />
+            {/* <SvgCoupon /> */}
+            <Icon name='confirmation number' color='black' />
             <Spacer size={10} />
             <List.Content>Coupon</List.Content>
           </div>
@@ -135,10 +73,16 @@ export default function AdminSidebar(props: SidebarProps) {
             </List.Item>
           </List>
         </Collapse>
+        <Spacer size={30} />
 
-        {/* Brands */}
+        {/* Brand */}
         <List.Item active={activeItem === 'brands'} style={styles.listItem}>
-          <List.Content>Brand</List.Content>
+          <div style={{display: "flex", alignItems: "center"}}>
+            {/* <SvgBrand /> */}
+            <Icon name='price tag' color='black' />
+            <Spacer size={10} />
+            <List.Content>Brand</List.Content>
+          </div>
         </List.Item>
         <Collapse expanded>
           <List padded selection verticalAlign="middle">
@@ -158,10 +102,15 @@ export default function AdminSidebar(props: SidebarProps) {
             </List.Item>
           </List>
         </Collapse>
+        <Spacer size={30} />
 
         {/* GPOINTS */}
         <List.Item active={activeItem === 'gpoints'} style={styles.listItem}>
-          <List.Content>GPoints</List.Content>
+          <div style={{display: "flex", alignItems: "center"}}>
+            <Icon name='global' color='black' />
+            <Spacer size={10} />
+            <List.Content>GPoints</List.Content>
+          </div>
         </List.Item>
         <Collapse expanded>
           <List padded selection verticalAlign="middle">
@@ -198,6 +147,6 @@ const styles = {
     paddingRight: '160px',
   },
   collapseItem: {
-    paddingLeft: '1.5rem',
+    paddingLeft: '2rem',
   },
 };
