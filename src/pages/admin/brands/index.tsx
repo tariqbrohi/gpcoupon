92b4c383ecbaa2bc7f89df6655eb4ccdb682690f
Seungjoo Kim <sjskybone@gmail.com>
2022-11-11 11:41:36 -0800

    GPN-280 Working on the Admin List Request Coupon Page.

--- src/pages/admin/brands/index.tsx
+++ src/pages/admin/brands/index.tsx
@@ -19,12 +19,6 @@ const Label = styled.label`
   width: 100px;
 `;
 
-const status = [
-  {value: "All"},
-  {value: "Active"},
-  {value: "Inactive"},
-];
-
 export default withPageAuthRequired(function Brands() {
   const { data: brands } = useGetBrandsQuery({
     data: {
@@ -36,89 +30,95 @@ export default withPageAuthRequired(function Brands() {
   const [searchBrand, setSearchBrand] = useState('');
   const [searchBusiness, setSearchBusiness] = useState('');
 
+  const status = [
+    {value: "All"},
+    {value: "Active"},
+    {value: "Inactive"},
+  ];
+
   return (
     <>
       <Head title='GPcoupon | List Brand' />
-        <AppMain>
-          <AdminLayout>
-            <section>
-              <Heading style={{color: "#2D126D"}}>List Brand</Heading>
-              <Spacer size={20} />
+      <AppMain>
+        <AdminLayout>
+          <section>
+            <Heading style={{color: "#2D126D"}}>List Brand</Heading>
+            <Spacer size={20} />
 
-              <Input
-                label="Brand Name"
-                icon="search menu"
-                value={searchBrand}
-                onChange={(e) => setSearchBrand(e.target.value)}
-                style={{width: "50%"}}
+            <Input
+              label="Brand Name"
+              icon="search menu"
+              value={searchBrand}
+              onChange={(e) => setSearchBrand(e.target.value)}
+              style={{width: "50%"}}
+            />
+            <Spacer size={20} />
+
+            <Input
+              label="Business Name"
+              icon="search outline"
+              value={searchBusiness}
+              onChange={(e) => setSearchBusiness(e.target.value)}
+              style={{width: "50%"}}
+            />
+            <Spacer size={20} />
+
+            <LabelContainer>
+              <div style={{display: "flex", }}>
+                <Input label='Create Date' placeholder='From' icon="calendar" iconPosition='right' style={{width: "50%"}} />
+                <Spacer size={46} />
+                <Input label='Create Date' placeholder='To' icon="calendar" iconPosition='right' style={{width: "50%"}} />
+              </div>
+            </LabelContainer>
+            <Spacer size={20} />
+
+            <LabelContainer>
+              <Label>Status</Label>
+              <Spacer size={10} />
+              <Dropdown
+                defaultValue={status[0].value}
+                direction="left"
+                options={status}
               />
-              <Spacer size={20} />
+            </LabelContainer>
+          </section>
+          <Spacer size={30} />
 
-              <Input
-                label="Business Name"
-                icon="search outline"
-                value={searchBusiness}
-                onChange={(e) => setSearchBusiness(e.target.value)}
-                style={{width: "50%"}}
-              />
-              <Spacer size={20} />
-
-              <LabelContainer>
-                <div style={{display: "flex", }}>
-                  <Input label='Create Date' placeholder='From' icon="calendar" iconPosition='right' style={{width: "50%"}} />
-                  <Spacer size={46} />
-                  <Input label='Create Date' placeholder='To' icon="calendar" iconPosition='right' style={{width: "50%"}} />
-                </div>
-              </LabelContainer>
-              <Spacer size={20} />
-
-              <LabelContainer>
-                <Label>Status</Label>
-                <Spacer size={10} />
-                <Dropdown
-                  defaultValue={status[0].value}
-                  direction="left"
-                  options={status}
-                />
-              </LabelContainer>
-            </section>
-            <Spacer size={30} />
-
-            <div style={{border: "2px solid #D9D9D9"}}></div>
-            <Spacer size={30} />
-
-            <List selection verticalAlign="middle">
-              {brands
-                ?.filter(({ name }) => {
-                  if (!searchBrand) return true;
+          <div style={{border: "2px solid #D9D9D9"}}></div>
+          <Spacer size={30} />
+  
+          <List selection verticalAlign="middle">
+            {brands
+              ?.filter(({ name }) => {
+                if (!searchBrand) return true;
+              
+                const similarity = stringSimilarity.compareTwoStrings(
+                  name,
+                  searchBrand,
+                );
                 
-                  const similarity = stringSimilarity.compareTwoStrings(
-                    name,
-                    searchBrand,
-                  );
-                  
-                  if (similarity > 0.25) return true;
-                  
-                  return false;
-                })
-                .map((brand) => (
-                  <List.Item
-                    key={brand.id}
-                    onClick={() =>
-                      Router.push(`${ROUTES.admin.brands}/${brand.id}`)
-                    }
-                  >
-                    <List.Image rounded src={brand.thumbnailUrl} size="mini" />
-                    <List.Content>
-                      <List.Heading>{brand.name}</List.Heading>
-                      <List.Heading>{brand.countries}</List.Heading>
-                    </List.Content>
-                  </List.Item>
-                ))
-              }
-            </List>
-          </AdminLayout>
-        </AppMain>
+                if (similarity > 0.25) return true;
+                
+                return false;
+              })
+              .map((brand) => (
+                <List.Item
+                  key={brand.id}
+                  onClick={() =>
+                    Router.push(`${ROUTES.admin.brands}/${brand.id}`)
+                  }
+                >
+                  <List.Image rounded src={brand.thumbnailUrl} size="mini" />
+                  <List.Content>
+                    <List.Heading>{brand.name}</List.Heading>
+                    <List.Heading>{brand.countries}</List.Heading>
+                  </List.Content>
+                </List.Item>
+              ))
+            }
+          </List>
+        </AdminLayout>
+      </AppMain>
     </>
   );
 });
--- src/pages/admin/items/index.tsx
+++ src/pages/admin/items/index.tsx
@@ -2,42 +2,97 @@ import AdminLayout from '@/layouts/AdminLayout';
 import React, { useState } from 'react';
 import Router from 'next/router';
 import stringSimilarity from 'string-similarity';
-import { Input, List, Spacer } from '@growth-ui/react';
+import { Dropdown, Heading, Input, List, Spacer } from '@growth-ui/react';
 import { ROUTES } from '@/ROUTES';
 import { useGetItemsQuery } from '@/services';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import Head from '@/modules/components/Head';
 import AppMain from '@/layouts/AppMain';
+import styled from 'styled-components';
+
+const LabelContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  width: 100px;
+`;
 
 export default withPageAuthRequired(function Items() {
   const { data: items } = useGetItemsQuery();
-  const [search, setSearch] = useState('');
+  const [searchCoupon, setSearchCoupon] = useState('');
+  const [searchMerchant, setSearchMerchant] = useState('');
+
+  const status = [
+    {value: "All"},
+    {value: "Active"},
+    {value: "Inactive"},
+  ];
 
   return (
     <>
       <Head title='GPcoupon | List Request Coupon' />
       <AppMain>
         <AdminLayout>
-          <Input
-            fluid
-            placeholder="Search"
-            icon="search outline"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
+          <section>
+            <Heading style={{color: "2D126D"}}>List Request Coupon</Heading>
+            <Spacer size={20} />
+
+            <Input
+              // fluid
+              label="Coupon Name"
+              icon="search menu"
+              value={searchCoupon}
+              onChange={(e) => setSearchCoupon(e.target.value)}
+              style={{width: "50%"}}
+            />
+            <Spacer size={20} />
+
+            <Input
+              label="Merchant Name"
+              icon="search outline"
+              value={searchMerchant}
+              onChange={(e) => setSearchMerchant(e.target.value)}
+              style={{width: "50%"}}
+            />
+            <Spacer size={20} />
+
+            <LabelContainer>
+              <div style={{display: "flex", }}>
+                <Input label='Create Date' placeholder='From' icon="calendar" iconPosition='right' style={{width: "50%"}} />
+                <Spacer size={46} />
+                <Input label='Create Date' placeholder='To' icon="calendar" iconPosition='right' style={{width: "50%"}} />
+              </div>
+            </LabelContainer>
+            <Spacer size={20} />
+
+            <LabelContainer>
+              <Label>Status</Label>
+              <Spacer size={10} />
+              <Dropdown
+                defaultValue={status[0].value}
+                direction="left"
+                options={status}
+              />
+            </LabelContainer>
+          </section>
           <Spacer size={30} />
+
+          <div style={{border: "2px solid #D9D9D9"}}></div>
+          <Spacer size={30} />
+
           <List selection verticalAlign="middle">
             {items
               ?.filter(({ name }) => {
-                if (!search) return true;
+                if (!searchCoupon) return true;
 
                 const similarity = stringSimilarity.compareTwoStrings(
                   name,
-                  search,
+                  searchCoupon,
                 );
 
                 if (similarity > 0.25) return true;
-
                 return false;
               })
               .map((item) => (
