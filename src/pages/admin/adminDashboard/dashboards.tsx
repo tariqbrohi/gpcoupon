c26cfbcbe4ac69ac127c0b218e47f85817b9700d
Seungjoo Kim <sjskybone@gmail.com>
2022-11-21 14:01:11 -0800

    GPN-275 Deleted unnecessary lines.

--- src/pages/admin/adminDashboard/dashboards.tsx
+++ src/pages/admin/adminDashboard/dashboards.tsx
@@ -5,7 +5,6 @@ import { Table, Spacer, Image } from "@growth-ui/react";
 import { ROUTES } from '@/ROUTES';
 import styled from "styled-components";
 import Router from 'next/router';
-import { useGetItemsQuery } from '@/services';
 
 const TableHeadCell = styled(Table.HeadCell)`
   text-align: center;
@@ -28,15 +27,7 @@ const TableCellLink = styled(Table.Cell)`
 `;
 
 export default function AdminDashboards(props: any) {
-    const { data: items } = useGetItemsQuery();
     const { total, orders } = props;
-
-    const addDays = (date: any, days: any) => {
-        const d = new Date(date);
-        d.setDate(d.getDate() + days);
-        
-        return d.toLocaleDateString();
-    }
 
     const calculateAmount = (totalAmount: number, oneAmount: number, originalPrice: number, amount: number) => {
         const qty = Math.round(totalAmount / oneAmount);
@@ -47,19 +38,6 @@ export default function AdminDashboards(props: any) {
                 <TableCell>${oneAmount}</TableCell>
                 <TableCell>${amount}</TableCell>
                 <TableCell>${amount * qty}</TableCell>
-                
-                {/* <TableCell>
-                    {qty}
-                </TableCell>
-                <TableCell>
-                    ${originalPrice * qty}
-                </TableCell>
-                <TableCell>
-                    ${oneAmount * qty}
-                </TableCell>
-                <TableCell>
-                    ${amount * qty}
-                </TableCell> */}
             </>
         );
     }
@@ -78,16 +56,6 @@ export default function AdminDashboards(props: any) {
                             <TableHeadCell>Retail Price</TableHeadCell>
                             <TableHeadCell>Merchant Profit</TableHeadCell>
                             <TableHeadCell>Merchant Total Profit</TableHeadCell>
-
-                            {/* <TableHeadCell>Logo</TableHeadCell>
-                            <TableHeadCell>Coupon Name</TableHeadCell>
-                            <TableHeadCell>Business Name (Should be changed to Merchant Name)</TableHeadCell>
-                            <TableHeadCell>Creation Date</TableHeadCell>
-                            <TableHeadCell>Expire Date</TableHeadCell>
-                            <TableHeadCell>Qty</TableHeadCell>
-                            <TableHeadCell>Original Price</TableHeadCell>
-                            <TableHeadCell>Retail Price</TableHeadCell>
-                            <TableHeadCell>Merchant Profit</TableHeadCell> */}
                         </Table.Row>
                     </Table.Head>
 
@@ -110,10 +78,7 @@ export default function AdminDashboards(props: any) {
                                         <TableCell>
                                             <Image size='small' src={order?._id.couponImageUrl} />
                                         </TableCell>
-                                        <TableCellLink 
-                                            // onClick={() => window.open(`${ROUTES.buy}/${order?._id.slug}/${order?._id.id}`)}
-                                            onClick={() => Router.push(`${ROUTES.admin.items}/${order?._id.id}`)}
-                                        >
+                                        <TableCellLink onClick={() => Router.push(`${ROUTES.admin.items}/${order?._id.id}`)}>
                                             {order?._id.name}                       
                                         </TableCellLink>
                                         <TableCell>
@@ -128,48 +93,6 @@ export default function AdminDashboards(props: any) {
                     ) : (
                         <></>
                     )}
-                    
-                    {/* {orders ? (
-                        <Table.Body>
-                            <Table.Row>
-                                <TableCell positive>Total</TableCell>
-                                <TableCell positive>-</TableCell>
-                                <TableCell positive>-</TableCell>
-                                <TableCell positive>-</TableCell>
-                                <TableCell positive>-</TableCell>
-                                <TableCell positive>{orders?.total.count || 0}</TableCell>
-                                <TableCell positive>-</TableCell>
-                                <TableCell positive>-</TableCell>
-                                <TableCell positive>${orders?.total.profitSum || 0}</TableCell>
-                            </Table.Row>
-                            
-                            {orders?.orders?.map((order: any, idx: number) => {
-                                return (
-                                    <Table.Row key={idx}>
-                                        <TableCell>
-                                            <Image size='small' src={order?.item.couponImageUrl} />
-                                        </TableCell>
-                                        <TableCellLink onClick={() => window.open(`${ROUTES.buy}/${order?.item.slug}/${order?.item.id}`)}>
-                                            {order?.item.name}                       
-                                        </TableCellLink>
-                                        <TableCell>
-                                            {order?.item?.brand?.name}
-                                        </TableCell>
-                                        <TableCell>
-                                            {new Date(Number(order?.createdAt) * 1000).toLocaleDateString()}
-                                        </TableCell>
-                                        <TableCell>
-                                            {addDays(order?.createdAt * 1000, order?.item?.expiresIn)}
-                                        </TableCell>
-                                        {calculateAmount(order?.payment?.totalAmount, order?.payment?.price.amount, order?.item?.originalPrice, order?.item?.amount)}
-                                    </Table.Row>
-                                )
-                            })}
-                        </Table.Body>
-                    ) : (
-                            <></>
-                        )
-                    } */}
                 </Table>
                 <Spacer size={20} />
                 
