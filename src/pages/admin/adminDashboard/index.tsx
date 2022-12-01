267776d176b82262b4a4c9bb5240ba4d810e007e
Seungjoo Kim <sjskybone@gmail.com>
2022-11-30 17:18:29 -0800

    GPN-275 Changed the contents of the admin and adminDashboard.

--- src/pages/admin/adminDashboard/index.tsx
+++ src/pages/admin/adminDashboard/index.tsx
@@ -1,24 +1,19 @@
-import Head from '@/modules/components/Head';
+/* eslint-disable react-hooks/rules-of-hooks */
 import AdminLayout from '@/layouts/AdminLayout';
-import Provider from '@/components/admin/items/Provider';
 import React, { useEffect, useState } from 'react';
-import { Button, DateInput, Heading, Input, Pagination, Select, Spacer } from '@growth-ui/react';
-import { useGetAffiliateItemsByAffiliateForAdminDashboardLazyQuery, useGetAffiliateItemsForAdminDashboardLazyQuery } from '@/services';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import AdminDashboards from './dashboards';
 import AppMain from '@/layouts/AppMain';
+import Head from '@/modules/components/Head';
+import Provider from '@/components/admin/items/Provider';
+import { Heading, Pagination, Select, Spacer } from '@growth-ui/react';
+import { useGetAffiliateItemsForAdminDashboardLazyQuery } from '@/services';
+import Input from '@growth-ui/react/elements/Input/Input';
 import styled from 'styled-components';
-import stringSimilarity from 'string-similarity';
+import DashboardGroupBy from './dashboardGroupBy';
 
 const LabelContainer = styled.div`
     display: flex;
     align-items: center;
-`;
-
-const DateContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 50%:
 `;
 
 const statusOption = [
@@ -26,13 +21,13 @@ const statusOption = [
         key: "All",
         value: "ALL",
         text: "All",
-    },
-    {
+    },  
+    {   
         key: "Available",
         value: "AVAILABLE",
         text: "Available",
-    },
-    {
+    },  
+    {   
         key: "Unavailable",
         value: "UNAVAILABLE",
         text: "Unavailable",
@@ -41,148 +36,77 @@ const statusOption = [
 
 const TAKE = 20;
 
-export default withPageAuthRequired(function AdminDashboard() {
-    const [ activePage, setActivePage ] = useState(1);
-    const [ startDate, setStartDate ] = useState('');
-    const [ endDate, setEndDate ] = useState('');
-    const [ couponName, setCouponName ] = useState('');
-    const [ status, setStatus ] = useState('ALL');
-    // const [ query, { data, loading }] = useGetAffiliateItemsForAdminDashboardLazyQuery({});
-    const [ query, { data, loading }] = useGetAffiliateItemsByAffiliateForAdminDashboardLazyQuery({});
-
+export default withPageAuthRequired(function index() {
+    const [search, setSearch] = useState('');
+    const [ sortBy, setSortBy ] = useState('createdAt, desc');
+    const [ activePage, setActivePage ] = useState(1)
+    const [ query, { data, loading }] = useGetAffiliateItemsForAdminDashboardLazyQuery({});
+    
     useEffect(() => {
         query({
             data: {
                 take: TAKE,
+                sortBy,
                 skip: (activePage - 1) * TAKE,
-                startDate,
-                endDate,
-                status,
             }
         });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [activePage, status]);
-
-    const handleSearchButton = () => {
-        if ((startDate !== '' && endDate) === '' || (startDate === '' && endDate !== '')) {
-            alert('Please submit From date and To date');
-
-            return;
-        }
+    }, [activePage, sortBy]);
     
-        query({
-            data: {
-                take: TAKE,
-                skip: (activePage - 1) * TAKE,
-                startDate,
-                endDate,
-                status,
-            }
-        });
-    }
-
     const handlePageChange = (_: any, { activePage }: any) => {
         setActivePage(activePage);
     };
-    
+
     return (
         <>
-            <Head title='GPcoupon | Admin Dashboard' />
+            <Head title='GPcoupon | Admin' />
             <AppMain>
                 <AdminLayout>
+
                     <Provider>
                         <Heading as="h2" style={{color: "#2D126D"}}>
                             Coupon Dashboard
                         </Heading>
                         <Spacer size={20} />
-                            
+
                         <section>
                             <LabelContainer>
-                                <Input 
-                                    label='Coupon Name' 
-                                    // label='Merchant Name'        // Should changed to this search name later
-                                    icon="search outline"
-                                    value={couponName}
-                                    onChange={(e) => setCouponName(e.target.value)}
-                                    style={{width: "50%"}} 
-                                />
+                              <Input 
+                                label='Business Name' 
+                                icon="search outline"
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                                style={{width: "50%"}} 
+                              />
                             </LabelContainer>
                             <Spacer size={20} />
 
                             <LabelContainer>
-                                <Select 
-                                    label='Status'
-                                    value={statusOption[0].value}
-                                    options={statusOption}
-                                    onChange={(_, data) => setStatus(data.newValues)}
-                                    style={{width: "50%"}}
-                                />
+                              <div style={{display: "flex", justifyContent: "space-between", width: "50%"}}>
+                                <Input label='Create Date' placeholder='From' icon="calendar" iconPosition='right' />
+                                <Input label='Create Date' placeholder='To' icon="calendar" iconPosition='right' />
+                              </div>
                             </LabelContainer>
                             <Spacer size={20} />
 
-                            <DateContainer>
-                                <LabelContainer style={{justifyContent: "space-between"}}>
-                                    <DateInput
-                                        mask="yyyy-mm-dd"
-                                        renderInput={(params) => 
-                                            <Input 
-                                                {...params} 
-                                                placeholder="yyyy-mm-dd" 
-                                                label='From'
-                                                // icon="calendar" 
-                                                // iconPosition='right' 
-                                                style={{width: "30%"}}
-                                            />
-                                        }
-                                        onChange={(_, date) => setStartDate(date)}
-                                    />
-                                    {/* <Spacer size={46} /> */}
-                                    <DateInput
-                                        mask="yyyy-mm-dd"
-                                        renderInput={(params) => 
-                                            <Input 
-                                                {...params} 
-                                                placeholder="yyyy-mm-dd" 
-                                                label='To'
-                                                // icon="calendar" 
-                                                // iconPosition='right' 
-                                                style={{width: "30%"}}
-                                            />
-                                        }
-                                        onChange={(_, date) => setEndDate(date)}
-                                    />
-                                    <Button rounded onClick={() => handleSearchButton()}>
-                                        Search
-                                    </Button>
-                                </LabelContainer>
-                            </DateContainer>
+                            <LabelContainer>
+                              <Select 
+                                label='Status'
+                                value={statusOption[0].value}
+                                options={statusOption}
+                                style={{minWidth: "13em"}}
+                              />
+                            </LabelContainer>
                         </section>
                         <Spacer size={30} />
 
                         <div style={{border: "2px solid #D9D9D9"}}></div>
 
                         <div style={{padding: "30px 0"}}>
-                            <AdminDashboards 
-                                startDate={startDate}
-                                endDate={endDate}
-                                total={data?.total}
-                                orders={data?.orders
-                                    ?.filter((o: any) => {
-                                        if (!couponName) return true;
-
-                                        const similarity = stringSimilarity.compareTwoStrings(
-                                            o._id.name,
-                                            couponName,
-                                        );
-                                          
-                                        if (similarity > 0.25) return true;
-                                          
-                                        return false;
-                                    })
-                                }
-                            />
+                            <DashboardGroupBy orders={data} />
+                            {/* <AdminDashboards orders={data} /> */}
                         </div>
-                        <Spacer size={30} />
+                        <Spacer size={20} />
 
                         <Pagination
                             totalPages={Math.ceil((data?.total?.count || 1) / TAKE)}
@@ -195,8 +119,3 @@ export default withPageAuthRequired(function AdminDashboard() {
         </>
     );
 });
-
-function e(e: any) {
-    throw new Error('Function not implemented.');
-}
-
