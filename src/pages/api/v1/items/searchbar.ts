5118ebca97294675da7ea354bc9b21d69d1b0973
kyuhakyuk <kyuhak.yuk@icloud.com>
2022-12-13 11:39:14 -0800

    ok

--- src/pages/api/v1/items/searchbar.ts
+++ src/pages/api/v1/items/searchbar.ts
@@ -1,46 +1,42 @@
-import errorHandler from "../../_middlewares/error-handler";
-import prisma from "@/prisma";
-import xoxoday from "../../_lib/xoxoday";
-import { NotFoundError, BadRequestError } from "@/lib/errors";
+import errorHandler from '../../_middlewares/error-handler';
+import prisma from '@/prisma';
+import xoxoday from '../../_lib/xoxoday';
+import { NotFoundError, BadRequestError } from '@/lib/errors';
 
 export default errorHandler(async function handler(req, res) {
-    const method = req.method;
+  const method = req.method;
 
-    if (method !== 'get') {
-        throw new NotFoundError();
-    }
+  if (method !== 'get') {
+    throw new NotFoundError();
+  }
 
-    const { 
-        searchQuery, 
-        country, 
-    } = req.query as any;
+  const { searchQuery, country } = req.query as any;
 
-    if (searchQuery.length < 4) {
-        throw new BadRequestError('Please use more than 4 letters for searchQuery');
-    }
+  if (searchQuery.length < 4) {
+    throw new BadRequestError('Please use more than 4 letters for searchQuery');
+  }
 
-    const itemsXoxo = await xoxoday.vouchers.findMany({
-        brand: searchQuery,
-        country,
-    });
+  const itemsXoxo = await xoxoday.vouchers.findMany({
+    brand: searchQuery,
+    country,
+  });
 
-    const items = await prisma.item.findMany({
-        where: 
-        {
-            name: 
-                {
-                    mode: 'insensitive',
-                    contains: searchQuery,
-                },
-            country,
-        },
-    });
-    
-    if (items.length > 0 && itemsXoxo && itemsXoxo.length > 0){
-        res.send(items.concat(itemsXoxo));
-    } else if (itemsXoxo && itemsXoxo.length > 0) {
-        res.send(itemsXoxo);
-    } else {
-        res.send(items);
-    }
-})
+  const items = await prisma.item.findMany({
+    where: {
+      name: {
+        mode: 'insensitive',
+        contains: searchQuery,
+      },
+      country,
+      affiliate: true,
+    },
+  });
+
+  if (items.length > 0 && itemsXoxo && itemsXoxo.length > 0) {
+    res.send(items.concat(itemsXoxo));
+  } else if (itemsXoxo && itemsXoxo.length > 0) {
+    res.send(itemsXoxo);
+  } else {
+    res.send(items);
+  }
+});
