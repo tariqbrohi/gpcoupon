d9d23358291e5c3f8bab9d5348ec56baf5c54bdc
Seungjoo Kim <sjskybone@gmail.com>
2022-12-20 12:18:16 -0800

    GPN-337 Updated the HomeContent list by country and removed pad-bot on the page.

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
 import Search from '@/modules/components/Search';
 import styled from 'styled-components';
 import { Chip, Grid, Spacer } from '@growth-ui/react';
 import { ROUTES } from '@/ROUTES';
+import AppContext from '@/modules/components/AppContext';
 
 const Container = styled.div`
   position: sticky;
@@ -19,28 +20,33 @@ const Container = styled.div`
 `;
 
 export default function HomeContent() {
+  const { country } = useContext(AppContext);
+
   return (
     <Container>
       {/* <Search /> */}
       <Spacer size={15} />
+
       <Grid.Row>
-        <Link href={ROUTES.gpcoupons}>
-          <a>
-            <Chip text="Categories" />
-          </a>
-        </Link>
-        <Spacer size={10} />
-        {/* <Link href={ROUTES.categories}>
-          <a>
-            <Chip text="Categories" />
-          </a>
-        </Link>
-        <Spacer size={10} />
-        <Link href={ROUTES.brands}>
-          <a>
-            <Chip text="Brands" />
-          </a>
-        </Link> */}
+        {country === 'US' ? (
+          <>
+            <Link href={ROUTES.gpcoupons}>
+              <a>
+                <Chip text="Categories" />
+              </a>
+            </Link>
+            <Spacer size={10} />
+          </>
+        ) : (
+          <>
+            <Link href={ROUTES.categories}>
+              <a>
+                <Chip text="Categories" />
+              </a>
+            </Link>
+            <Spacer size={10} />
+          </>
+        )}
       </Grid.Row>
     </Container>
   );
