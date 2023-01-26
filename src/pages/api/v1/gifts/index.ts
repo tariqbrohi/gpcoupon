2791b63f7d6d524152fad5a0179b8b28d1daeb07
kyuhakyuk <kyuhak.yuk@icloud.com>
2023-01-26 15:20:08 -0800

    ok

--- src/pages/api/v1/gifts/index.ts
+++ src/pages/api/v1/gifts/index.ts
@@ -1,41 +1,38 @@
 import errorHandler from '@/pages/api/_middlewares/error-handler';
 import prisma from '@/prisma';
-import withApiAuthRequired from '../../_middlewares/with-api-auth-required';
 import { NotFoundError } from '@/lib/errors';
 
-export default withApiAuthRequired(
-  errorHandler(async function handler(req, res) {
-    if (req.method !== 'get') {
-      throw new NotFoundError();
-    }
+export default errorHandler(async function handler(req, res) {
+  if (req.method !== 'get') {
+    throw new NotFoundError();
+  }
 
-    const { key } = req.query;
+  const { key } = req.query;
 
-    const orderWithGifts = await prisma.gPointOrder.findFirst({
-      where: {
-        uniqueId: key as string,
-      },
-      include: {
-        gifts: {
-          include: {
-            order: true,
-          },
+  const orderWithGifts = await prisma.gPointOrder.findFirst({
+    where: {
+      uniqueId: key as string,
+    },
+    include: {
+      gifts: {
+        include: {
+          order: true,
         },
       },
-    });
+    },
+  });
 
-    if (!orderWithGifts) {
-      throw new NotFoundError();
-    }
+  if (!orderWithGifts) {
+    throw new NotFoundError();
+  }
 
-    const data = orderWithGifts.gifts.map((gift) => ({
-      id: gift.id,
-      orderid: gift.orderId,
-      code: gift.code,
-      pin: gift.pin,
-      gpoint: gift.order.gpoint,
-    }));
+  const data = orderWithGifts.gifts.map((gift) => ({
+    id: gift.id,
+    orderid: gift.orderId,
+    code: gift.code,
+    pin: gift.pin,
+    gpoint: gift.order.gpoint,
+  }));
 
-    res.send(data);
-  }),
-);
+  res.send(data);
+});
